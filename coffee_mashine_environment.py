889f529df5d1f8d1bcc4ed5cf387d05c3f9b91af
Anna <an.vasylkivska@gmail.com>
2020-06-15 17:01:41 +0300

    On a coffee loop
    Programed the machine to display on the screen the amount of supplies left. Set the main loop: now the menu keeps updating until you enter “exit”.
    
    Objectives:
    Write a program that will work endlessly to make coffee for all interested persons until the shutdown signal is given. Introduce two new options: "remaining" and "exit".
    
    Do not forget that you can be out of resources for making coffee. If the coffee machine doesn't have enough resources to make coffee, the program should output a message that says it can't make a cup of coffee.
    
    And the last improvement to the program at this step — if the user types "buy" to buy a cup of coffee and then changes his mind, they should be able to type "back" to return into the main cycle.

--- coffee_mashine_environment.py
+++ coffee_mashine_environment.py
@@ -8,41 +8,62 @@ coffee = ['espresso', 'latte', 'cappuccino']
 
 def buying_coffee():
     print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:')
-    coffee_type = (int(input()))
-    if coffee_type == 1:
+    coffee_type = (input())
+    if coffee_type == 'back':
+        main_process()
+    elif int(coffee_type) == 1:
         make_coffee(250, 0, 16, 1, 4)
-    elif coffee_type == 2:
+    elif int(coffee_type) == 2:
         make_coffee(350, 75, 20, 1, 7)
-    elif coffee_type == 3:
+    elif int(coffee_type) == 3:
         make_coffee(200, 100, 12, 1, 6)
     else:
         print('I can not make this coffee type')
 
 
 def make_coffee(a, b, c, d, e):
-    global water_av
-    global milk_av
-    global coffee_av
-    global cups_av
-    global money
+    global water_av, milk_av, coffee_av, cups_av, money
     water_av -= a
     milk_av -= b
     coffee_av -= c
     cups_av -= d
     money += e
-    print('The coffee machine has:')
-    print(water_av, 'of water')
-    print(milk_av, 'of milk')
-    print(coffee_av, 'of coffee beans')
-    print(cups_av, 'of disposable cups')
-    print(money, 'of money')
+    if water_av >= 0 and milk_av >= 0 and coffee_av >= 0 and cups_av >= 0:
+        print('I have enough resources, making you a coffee!')
+    elif water_av < 0:
+        print('Sorry, not enough water!')
+        water_av += a
+        milk_av += b
+        coffee_av += c
+        cups_av += d
+        money -= e
+    elif milk_av < 0:
+        print('Sorry, not enough milk!')
+        water_av += a
+        milk_av += b
+        coffee_av += c
+        cups_av += d
+        money -= e
+    elif coffee_av < 0:
+        print('Sorry, not enough coffee!')
+        water_av += a
+        milk_av += b
+        coffee_av += c
+        cups_av += d
+        money -= e
+    elif cups_av < 0:
+        print('Sorry, not enough cups!')
+        water_av += a
+        milk_av += b
+        coffee_av += c
+        cups_av += d
+        money -= e
+    print()
+    main_process()
 
 
 def filling():
-    global water_av
-    global milk_av
-    global coffee_av
-    global cups_av
+    global water_av, milk_av, coffee_av, cups_av, money
     print('Write how many ml of water do you want to add:')
     water_filled = int(input())
     water_av += water_filled
@@ -55,43 +76,47 @@ def filling():
     print('Write how many disposable cups of coffee do you want to add:')
     cups_filled = int(input())
     cups_av += cups_filled
+    main_process()
 
 
 def taking():
-    global water_av
-    global milk_av
-    global coffee_av
-    global cups_av
     global money
     print('I gave you $', money, sep='')
     money = 0
     print()
+    main_process()
+
+
+def remaining():
     print('The coffee machine has:')
     print(water_av, 'of water')
     print(milk_av, 'of milk')
     print(coffee_av, 'of coffee beans')
     print(cups_av, 'of disposable cups')
     print(money, 'of money')
+    print()
+    main_process()
 
 
-print('The coffee machine has:')
-print(water_av, 'of water')
-print(milk_av, 'of milk')
-print(coffee_av, 'of coffee beans')
-print(cups_av, 'of disposable cups')
-print(money, 'of money')
-print()
-print('Write action (buy, fill, take)')
-action = input()
-if action == 'buy':
-    buying_coffee()
-elif action == 'fill':
-    filling()
-    print('The coffee machine has:')
-    print(water_av, 'of water')
-    print(milk_av, 'of milk')
-    print(coffee_av, 'of coffee beans')
-    print(cups_av, 'of disposable cups')
-    print(money, 'of money')
-elif action == 'take':
-    taking()
+def main_process():
+    print('Write action (buy, fill, take, remaining, exit)')
+    action = input()
+    if action == 'buy':
+        buying_coffee()
+    elif action == 'fill':
+        filling()
+        print('The coffee machine has:')
+        print(water_av, 'of water')
+        print(milk_av, 'of milk')
+        print(coffee_av, 'of coffee beans')
+        print(cups_av, 'of disposable cups')
+        print(money, 'of money')
+    elif action == 'take':
+        taking()
+    elif action == 'remaining':
+        remaining()
+    elif action == 'exit':
+        exit()
+
+
+main_process()
